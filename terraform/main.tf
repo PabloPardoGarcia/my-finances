6caa32797b0bc0d9c4f34caed333b73aa63b79c1
GithubActions <infra+githubactions@eyeem.com>
2023-11-06 10:53:50 +0100

    simplify main.tf by moving poastres to my-finances module

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,44 +9,10 @@ module "local-storage-class" {
   local_storage_class_name = "local-storage-class"
 }
 
-module "my-finances-postgres-volume" {
-  source = "./modules/local_volume"
-  namespace = kubernetes_namespace.my-finances-namespace.metadata.0.name
-  pvc_name = "my-finances-postgres-pvc"
-  pv_name = "my-finances-postgres-pv"
-  pv_path = abspath("../db")
-  pv_capacity = "500Mi"
-  pv_node_names = ["docker-desktop"]
-  storage_class_name = module.local-storage-class.storage-class-name
-}
-
-module "postgres" {
-  source = "./modules/postgres"
-
-  postgres_namespace = kubernetes_namespace.my-finances-namespace.metadata.0.name
-  postgres_pvc_name = module.my-finances-postgres-volume.pvc-name
-  postgres_pv_name = module.my-finances-postgres-volume.pv-name
-  postgres_name = "my-finances-postgres"
-}
-
-module "my-finances-dbt-volume" {
-  source = "./modules/local_volume"
-  namespace = kubernetes_namespace.my-finances-namespace.metadata.0.name
-  pvc_name = "my-finances-dbt-pvc"
-  pv_name = "my-finances-dbt-pv"
-  pv_path = abspath("../dbt")
-  pv_capacity = "200Mi"
-  pv_node_names = ["docker-desktop"]
-  storage_class_name = module.local-storage-class.storage-class-name
-}
-
 module "my_finances" {
   source = "./modules/my_finances"
   namespace = kubernetes_namespace.my-finances-namespace.metadata.0.name
 
-  dbt_deployment_name = "my-finances-dbt"
-  dbt_pvc_name = module.my-finances-dbt-volume.pvc-name
-  dbt_pv_name = module.my-finances-dbt-volume.pv-name
   dbt_pv_path_to_app = "my_finances"
   dbt_pv_path_to_profiles = "profiles.yml"
   dbt_image = "custom-dbt-postgres-1.6.6"
--- terraform/modules/my_finances/main.tf
+++ terraform/modules/my_finances/main.tf
@@ -1,22 +1,53 @@
+module "my-finances-postgres-volume" {
+  source = "../local_volume"
+  namespace = var.namespace
+  pvc_name = "my-finances-postgres-pvc"
+  pv_name = "my-finances-postgres-pv"
+  pv_path = abspath("../db")
+  pv_capacity = "500Mi"
+  pv_node_names = ["docker-desktop"]
+  storage_class_name = var.storage_class_name
+}
+
+module "postgres" {
+  source = "../postgres"
+  postgres_namespace = var.namespace
+  postgres_pvc_name = module.my-finances-postgres-volume.pvc-name
+  postgres_pv_name = module.my-finances-postgres-volume.pv-name
+  postgres_name = "my-finances-postgres"
+  postgres_labels = var.labels
+}
+
+module "my-finances-dbt-volume" {
+  source = "../local_volume"
+  namespace = var.namespace
+  pvc_name = "my-finances-dbt-pvc"
+  pv_name = "my-finances-dbt-pv"
+  pv_path = abspath("../dbt")
+  pv_capacity = "200Mi"
+  pv_node_names = ["docker-desktop"]
+  storage_class_name = var.storage_class_name
+}
+
 resource "kubernetes_deployment_v1" "my-finances-dbt-deployment" {
   metadata {
-    name = var.dbt_deployment_name
+    name = "my-finances-dbt"
     namespace = var.namespace
   }
   spec {
     replicas = "1"
     selector {
-      match_labels = var.dbt_labels
+      match_labels = var.labels
     }
     template {
       metadata {
-        labels = var.dbt_labels
+        labels = var.labels
       }
       spec {
         volume {
-          name = var.dbt_pv_name
+          name = module.my-finances-dbt-volume.pv-name
           persistent_volume_claim {
-            claim_name = var.dbt_pvc_name
+            claim_name = module.my-finances-dbt-volume.pvc-name
           }
         }
         container {
@@ -47,13 +78,13 @@ resource "kubernetes_deployment_v1" "my-finances-dbt-deployment" {
 
           volume_mount {
             mount_path = "/usr/app"
-            name       = var.dbt_pv_name
+            name       = module.my-finances-dbt-volume.pv-name
             sub_path   = var.dbt_pv_path_to_app
           }
 
           volume_mount {
             mount_path = "/root/.dbt/profiles.yml"
-            name       = var.dbt_pv_name
+            name       = module.my-finances-dbt-volume.pv-name
             sub_path   = var.dbt_pv_path_to_profiles
           }
 
@@ -65,9 +96,9 @@ resource "kubernetes_deployment_v1" "my-finances-dbt-deployment" {
 
 resource "kubernetes_service_v1" "my-finances-dbt-service" {
   metadata {
-    name = var.dbt_deployment_name
+    name = "my-finances-dbt"
     namespace = var.namespace
-    labels = var.dbt_labels
+    labels = var.labels
   }
   spec {
     type = "NodePort"
@@ -76,6 +107,6 @@ resource "kubernetes_service_v1" "my-finances-dbt-service" {
       port = 80
       target_port = "docs-port"
     }
-    selector = var.dbt_labels
+    selector = var.labels
   }
 }
